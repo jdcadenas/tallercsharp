690846149f6745635a45344e3862e7bbe3ba7617
jdcadenas <jdcadenas@gmail.com>
2026-04-17 11:29:57 -0400

    agregando maniṕulacion de texto

--- TallerIujo01/TallerIujo01/Program.cs
+++ TallerIujo01/TallerIujo01/Program.cs
@@ -14,12 +14,30 @@ namespace TallerIujo01
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
 			
-			// TODO: Implement Functionality Here
+			Console.WriteLine("===Taller 01 ===");
+			
+			// 1. El dato del usuario
+			string registroUsuario = "    ID_777;   EVALUACION; 95";
+			
+			Console.WriteLine(registroUsuario);
+			string registroLimpio = registroUsuario.Trim();
+			Console.WriteLine(registroLimpio);
+			
+			string[] partes = registroLimpio.Split(';');
+			string id = partes[0].Trim();
+			string nombre = partes[1].Trim();
+			string nota = partes[2].Trim();
+			
+			Console.WriteLine(string.Format("el id es: {0} del usuario {1} con la nota {2}",id,nombre, nota));
+			
+			
+			
+			
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
+			
 		}
 	}
 }
