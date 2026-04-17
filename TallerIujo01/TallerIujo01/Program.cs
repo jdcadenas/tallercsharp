29420a2eea1cc9eca09d9b87f48914b30b7b9158
jdcadenas <jdcadenas@gmail.com>
2026-04-17 10:55:29 -0400

    crando proyecto

---
+++ TallerIujo01/TallerIujo01/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Usuario
+ * Date: 17/4/2026
+ * Time: 10:53 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TallerIujo01
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
